a541093174220455583fdfb60bbbe6c7f3b5f1aa
hritvik-otc <165265766+hritvik-otc@users.noreply.github.com>
2024-04-23 13:13:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+//Investigative Padhai
+
+const accountID = 144553
+let accountEmail = "hritvik@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" //never to use but this is possible.
+let accountState; //undefined
+
+// accountID = 2 -> not allowed
+accountEmail = "hsb@hsb.com"
+accountPassword = "212121222111"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity,accountState])
+
+/* MultiLine Comments
+    Prefer not to use var  -> because of issue in block scope and functional scope.
+*/
+
