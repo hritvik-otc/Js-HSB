199206551ba02d0053841ac84a6eb288f8513f01
hritvik-otc <165265766+hritvik-otc@users.noreply.github.com>
2024-04-30 05:58:29 +0000

    datatypes and typeof

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+
+
+// PRIMITIVE
+// number => 2 ^ 53 (approx)
+// we have bigInt
+// string => ""
+// boolean => true/false
+// null => it is a standalone value.
+// undefined => value is not assigned yet.
+// symbol => will be helpul to find uniqueness in components in react
+
+//OBJECTS
+console.log(typeof "hritvik") ; //String
+console.log(typeof null) ; // Object
+console.log(typeof undefined) ; // undefined - type in JS
